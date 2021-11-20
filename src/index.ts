9a6b6a4ea5496a0af1100148c876cc81000f3d21
Aditya Chaphekar <aditya@hubilo.com>
2021-11-20 20:14:18 +0530

    Logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from 'morgan';
 import cors from "cors";
 
 import {errorHandler,notFoundHandler} from "./middleware/error.middleware";
@@ -9,6 +10,7 @@ import {routes} from "./routes";
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
