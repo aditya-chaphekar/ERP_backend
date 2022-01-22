0c5a91eeaeff6e0141bcc5781b57b8a333880b19
Aditya <adityachaps@gmail.com>
2022-01-22 19:27:04 +0530

    Demo App Routes added

--- src/api/Demo/demo.router.ts
+++ src/api/Demo/demo.router.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import {add, list, listOne} from "./demo.service";
+import {add, list, listOne, remove, update} from "./demo.service";
 import {checkBlank, validateEmail} from "../../common/validation";
 import {failedResponse, successResponse} from "../../common/responses";
 import {HttpConstants} from "../../constants/http-constants";
@@ -41,6 +41,49 @@ DemoRouter.post("/add", async (req: Request, res: Response) => {
 
     try {
         const {code, message, data} = await add({
+            name,
+            classVar,
+            email,
+            password,
+        });
+        successResponse(res,{code, message, data});
+    } catch (e) {
+        failedResponse(res, e)
+    }
+})
+
+DemoRouter.post("/delete", async (req: Request, res: Response) => {
+    // tslint:disable-next-line:one-variable-per-declaration
+    const id = req.body.id;
+
+    if (checkBlank([id]))
+        return successResponse(res, {...HttpConstants.BAD_REQUEST, ...ErrorConstants.MISSING_PARAM })
+
+    try {
+        const {code, message, data} = await remove({id});
+        successResponse(res,{code, message, data});
+    } catch (e) {
+        failedResponse(res, e)
+    }
+})
+
+DemoRouter.post("/update",async (req: Request, res: Response) => {
+    // tslint:disable-next-line:one-variable-per-declaration
+    const id = req.body.id,
+        name = req.body.name,
+        email = req.body.email,
+        password = req.body.password,
+        classVar = req.body.classVar;
+
+    if (checkBlank([id, name, email, password, classVar]))
+        return successResponse(res, {...HttpConstants.BAD_REQUEST, ...ErrorConstants.MISSING_PARAM })
+
+    if(!validateEmail(email))
+        return successResponse(res, {...HttpConstants.BAD_REQUEST, ...ErrorConstants.INVALID_EMAIL })
+
+    try {
+        const {code, message, data} = await update({
+            id,
             name,
             classVar,
             email,
--- src/api/Demo/demo.service.ts
+++ src/api/Demo/demo.service.ts
@@ -69,3 +69,43 @@ export const add = async ({name, classVar, email, password}): Promise<StudentLog
         };
     }
 }
+export const remove = async  ({id}: ListOne): Promise<StudentLogin> => {
+    try {
+        const data = await getRepository(Student).delete({id: parseInt(id,10)});
+        if (data) {
+            return {
+                code: HttpConstants.SUCCESS.code,
+                message: HttpConstants.SUCCESS.message,
+            };
+        } else
+            return {
+                code: HttpConstants.BAD_REQUEST.code,
+                message: DemoConstants.NO_DATA,
+            };
+    } catch (e) {
+        return {
+            code: HttpConstants.BAD_REQUEST.code,
+            message: e,
+        };
+    }
+}
+export const update = async ({id,name, classVar, email, password}): Promise<StudentLogin> => {
+    try {
+        const data = await getRepository(Student).update({id: parseInt(id,10)}, {name, classVar, email, password});
+        if (data) {
+            return {
+                code: HttpConstants.SUCCESS.code,
+                message: HttpConstants.SUCCESS.message,
+            };
+        } else
+            return {
+                code: HttpConstants.BAD_REQUEST.code,
+                message: DemoConstants.NO_DATA,
+            };
+    } catch (e) {
+        return {
+            code: HttpConstants.BAD_REQUEST.code,
+            message: e,
+        };
+    }
+}
